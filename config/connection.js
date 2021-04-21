e7f1fe7cd0e2f02f44d5abbb9db474995107c76b
Savannah Bennett <savvy.bennett8@gmail.com>
2021-04-20 18:05:47 -0600

    fixed configuration

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,8 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhose',
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
+        host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
