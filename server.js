135396856e995c1271304b850cbe1512f11b91d2
Savannah Bennett <savvy.bennett8@gmail.com>
2021-04-20 19:13:50 -0600

    homepage template populated with sequelize data

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+//express-handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//serve up public files
+app.use(express.static(path.join(__dirname, 'public')));
+//express-handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //turn on routes
 app.use(routes);
