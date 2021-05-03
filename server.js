c0625a5aea58efee68dd6cc104ba1839732a668d
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-02 22:05:16 -0600

    format_date implemented in partials. fixed sql auth.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+//helpers
+const helpers = require('./utils/helper');
 //express-handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
