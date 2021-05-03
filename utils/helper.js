78456f760f387b87ed9fa191e5a43d34e42e4eab
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-02 21:48:45 -0600

    helper tests & functions complete

--- utils/helper.js
+++ utils/helper.js
@@ -9,5 +9,13 @@ module.exports = {
             return `${word}s`;
         }
         return word;
+    },
+    format_url: url => {
+        return url
+            .replace('http://', '')
+            .replace('https://', '')
+            .replace('www.', '')
+            .split('/')[0]
+            .split('?')[0];
     }
 }
