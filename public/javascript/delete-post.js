9b15f41475283d4341a9da99fbd45f96c00bcc5d
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-03 19:18:22 -0600

    delete post functional

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,20 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    //take the url string and split into an array based on '/' then grab the last item (post_id)
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
